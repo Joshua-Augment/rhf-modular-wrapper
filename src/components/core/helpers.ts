583a866d37c4732e876333df66a6659574d2e69e
JJSamuel343 <joshuajs92@gmail.com>
2023-04-19 16:52:49 +0800

    Added optional to compareArrays

--- src/components/core/helpers.ts
+++ src/components/core/helpers.ts
@@ -2,7 +2,7 @@ export const compareArrays = (a:any[], b:any) => {
   if (!Array.isArray(a) || !Array.isArray(b)) {return a === b}
   console.log("[compareArrays] - a",a)
   console.log("[compareArrays] - b",b)
-  const res =  a.length === b.length && a.every((element, index) => Array.isArray(element) ? compareArrays(element,b[index]) : element === b[index]);
+  const res =  (a?.length === b?.length) && a.every((element, index) => Array.isArray(element) ? compareArrays(element,b[index]) : element === b[index]);
   console.log("[compareArrays] - result",res)
   return res
 }
