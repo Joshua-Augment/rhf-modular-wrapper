f63e90966d0f8fc86b88252866f5cc2028630fe0
JJSamuel343 <joshuajs92@gmail.com>
2024-03-15 14:36:50 +0800

    updated and remove react-pdf

--- dist/esm/components/Inputs/Uploaders/Dropzone/components/PreviewModal/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface IPreviewModal {
-    file: null | File;
-    setFile: (a: null) => void;
-}
-declare const PreviewModal: (props: IPreviewModal) => React.JSX.Element;
-export default PreviewModal;
